f24521ad8734a5314d2f38bc4f6348102787b8ab
Glor Shin <sykkkkk412@naver.com>
2022-11-15 21:51:42 +0900

    fix: app.js gql찾을 수 없는 버그 해결

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import { useQuery, useMutation } from '@apollo/client';
-import { GET_WORDS, ADD_WORDS, DELETE_WORDS } from './gql/words.js';
 import Note from './containers/Note.js';
 
 function App() {
-  function execDeleteWord() {
-    deleteWord({variables: {id: "1"}})
-  }
+  // function execDeleteWord() {
+  //   deleteWord({variables: {id: "1"}})
+  // }
+  //
+  // const [deleteWord] = useMutation(DELETE_WORDS, { oncCompleted: deleteTeamCompleted })
+  // function deleteTeamCompleted() {
+  //   console.log('delete');
+  // }
 
-  const [deleteWord] = useMutation(DELETE_WORDS, { oncCompleted: deleteTeamCompleted })
-  function deleteTeamCompleted() {
-    console.log('delete');
-  }
 
 
-
-  const { loading, error, data } = useQuery(GET_WORDS);
+  // const { loading, error, data } = useQuery(GET_WORDS);
   // if (loading) return <p>Loading...</p>;
   // if (error) return <p>Error :(</p>;
-  console.log(data)
   return (
     <div className="App">
-      <button onClick={execDeleteWord}>삭제</button>
+      단어추가
+      {/*<button onClick={execDeleteWord}>삭제</button>*/}
     </div>
   );
 }
